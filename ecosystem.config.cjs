7e2025e6f5b3c572738e956f8a24f428e18848ca
nico <18157180412@163.com>
2026-05-13 11:07:50 +0800

    deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 9999;
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const INTERPRETER_PATH = process.env.INTERPRETER_PATH || '/home/ubuntu/.nvm/versions/node/v24.15.0/bin/node';
 
 module.exports = {
   apps: [
@@ -8,8 +10,9 @@ module.exports = {
       name: `lifeos-${PORT}`,
       cwd: '.',
       script: '.output/server/index.mjs',
+      interpreter: INTERPRETER_PATH,
       args: `dev --port ${PORT}`,
-      env: { NODE_ENV: 'development','PORT': PORT },
+      env: { NODE_ENV: NODE_ENV, 'PORT': PORT },
       watch: false,
     },
   ],
