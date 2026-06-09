4a06ae726d02ebba53e5d28e481314835833c45f
nico <18157180412@163.com>
2026-06-09 10:10:48 +0800

    [任务] 报错修复 (2分钟43秒)

--- app-modules/billing/components/ImportRecordDetail.vue
+++ app-modules/billing/components/ImportRecordDetail.vue
@@ -1,12 +1,7 @@
 <template>
   <Teleport to="body">
     <Transition name="record-modal">
-      <div
-        v-if="visible"
-        ref="overlayRef"
-        class="record-modal-overlay"
-        :style="overlayZIndex ? { zIndex: overlayZIndex } : undefined"
-      >
+      <div v-if="visible" ref="overlayRef" class="record-modal-overlay" :style="zStyle">
         <div class="record-modal-card" @click.stop>
           <div class="record-modal-header">
             <div class="header-left">
@@ -278,6 +273,7 @@ const props = defineProps<{
   categories: BillCategory[]
 }>()
 const overlayZIndex = useZIndexOnOpen(() => props.visible)
+const zStyle = computed(() => overlayZIndex.value ? { zIndex: overlayZIndex.value } : undefined)
 
 const emit = defineEmits<{
   (e: 'close'): void
